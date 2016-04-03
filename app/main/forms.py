7c79714be65b22d9aa955998ce4742c23c9a90cd
taogeT <zwtzjd@gmail.com>
2016-04-03 15:46:14 +0800

    miss upload app.main.forms file.

---
+++ app/main/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField, BooleanField, SelectField
+from wtforms.validators import Required, Length
+
+
+class SearchRoomForm(Form):
+    site_name = SelectField('站点', validators=[Length(0, 64)])
+    boardcaster = StringField('主播名', validators=[Length(0, 64)])
+    room_name = StringField('房间名', validators=[Length(0, 128)])
+    only_active = BooleanField('正在直播', default=True)
+    submit = SubmitField('搜索')
