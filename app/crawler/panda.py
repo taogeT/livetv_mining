f6fdabeacbf75473cbfbcc3d3bad2aae53a82d82
taogeT <zwtzjd@gmail.com>
2016-04-29 04:15:52 +0800

    crawl module panda from selenium to requests.

--- app/crawler/panda.py
+++ app/crawler/panda.py
@@ -1,17 +1,20 @@
 # -*- coding: UTF-8 -*-
 from flask import current_app
 from datetime import datetime
-from selenium.common.exceptions import TimeoutException
+from lxml import etree
 
 from .. import db
 from . import LiveTVCrawler, LiveTVSite, LiveTVChannel, LiveTVRoom, \
-              LiveTVChannelData, LiveTVRoomData, get_webdriver_client
+              LiveTVChannelData, LiveTVRoomData, request_headers
 
-import json
+import requests
+import copy
 
-CHANNEL_API = 'http://api.m.panda.tv/ajax_get_all_subcate'
+CHANNEL_API = 'http://www.panda.tv/cate'
 ROOM_LIST_API = 'http://www.panda.tv/ajax_sort?classification={}&pageno={}&pagenum={}'
 ROOM_API = 'http://api.m.panda.tv/ajax_get_liveroom_baseinfo?roomid={}'
+panda_headers = copy.deepcopy(request_headers)
+panda_headers['Host'] = 'www.panda.tv'
 
 
 class PandaCrawler(LiveTVCrawler):
@@ -29,133 +32,111 @@ class PandaCrawler(LiveTVCrawler):
 
     def _channels(self, site):
         current_app.logger.info('调用频道接口:{}'.format(CHANNEL_API))
-        webdriver_client = get_webdriver_client()
-        try:
-            try:
-                webdriver_client.get(CHANNEL_API)
-                pre_element = webdriver_client.find_element_by_tag_name('pre')
-            except TimeoutException:
-                current_app.logger.error('调用接口失败: 内容获取失败')
-                return False
-            try:
-                respjson = json.loads(pre_element.get_attribute('innerHTML'))
-            except ValueError:
-                current_app.logger.error('调用接口失败: 内容解析json失败')
-                return False
-            if respjson['errno'] != 0:
-                current_app.logger.error('调用接口失败: 返回错误结果 {}'.format(respjson['errmsg']))
-                return False
-            site.channels.update({'valid': False})
-            for channel_json in respjson['data']:
-                channel_json['url'] = '{}/{}'.format(site.url, channel_json['ename'])
-                channel = site.channels.filter_by(short_name=channel_json['ename']).one_or_none()
-                if not channel:
-                    channel = LiveTVChannel(short_name=channel_json['ename'])
-                    current_app.logger.info('新增频道 {}: {}'.format(channel_json['cname'], channel_json['url']))
-                else:
-                    current_app.logger.info('更新频道 {}: {}'.format(channel_json['cname'], channel_json['url']))
-                channel.site = site
-                channel.name = channel_json['cname']
-                channel.url = channel_json['url']
-                channel.image_url = channel_json['img']
-                channel.icon_url = channel_json['img']
-                channel.valid = True
-                db.session.add(channel)
-            site.last_crawl_date = datetime.utcnow()
-            db.session.add(site)
-            db.session.commit()
-            return True
-        finally:
-            webdriver_client.close()
-            webdriver_client.quit()
+        resp = requests.get(CHANNEL_API, headers=panda_headers)
+        if resp.status_code != requests.codes.ok:
+            current_app.logger.error('调用接口{}失败: 状态{}'.format(CHANNEL_API, resp.status_code))
+            return False
+        site.channels.update({'valid': False})
+        htmlroot = etree.HTML(resp.content)
+        dirul = htmlroot.xpath('//ul[contains(@class,\'video-list\')]')[0]
+        for channel_a_element in dirul.xpath('./li/a'):
+            img_element = channel_a_element.xpath('./div[@class=\'img-container\']/img')[0]
+            channel_img_url = img_element.get('src')
+            div_element = channel_a_element.xpath('./div[@class=\'cate-title\']')[0]
+            channel_name = div_element.text
+            channel_url = channel_a_element.get('href')
+            channel_short_name = channel_url.split('/')[-1]
+            channel_url = '{}/{}'.format(site.url, channel_url)
+            channel = site.channels.filter_by(short_name=channel_short_name).one_or_none()
+            if not channel:
+                channel = LiveTVChannel(short_name=channel_short_name)
+                current_app.logger.info('新增频道 {}: {}'.format(channel_name, channel_url))
+            else:
+                current_app.logger.info('更新频道 {}: {}'.format(channel_name, channel_url))
+            channel.site = site
+            channel.name = channel_name
+            channel.url = channel_url
+            channel.image_url = channel_img_url
+            channel.icon_url = channel_img_url
+            channel.valid = True
+        site.last_crawl_date = datetime.utcnow()
+        db.session.add(site)
+        db.session.commit()
+        return True
 
     def _rooms(self, channel):
         current_app.logger.info('开始扫描频道 {}: {}'.format(channel.name, channel.url))
         channel.rooms.update({'last_active': False})
         crawl_pageno, crawl_pagenum = 1, 120
         crawl_room_count = 0
-        webdriver_client = get_webdriver_client()
-        try:
-            while True:
-                requrl = ROOM_LIST_API.format(channel.short_name, crawl_pageno, crawl_pagenum)
-                try:
-                    webdriver_client.get(requrl)
-                    body_element = webdriver_client.find_element_by_tag_name('body')
-                except TimeoutException:
-                    current_app.logger.error('调用频道接口失败: 内容获取失败')
-                    return False
-                try:
-                    respjson = json.loads(body_element.get_attribute('innerHTML'))
-                except ValueError:
-                    current_app.logger.error('调用接口{}失败: 内容解析json失败'.format(requrl))
-                    return False
-                if respjson['errno'] != 0:
-                    current_app.logger.error('调用接口{}失败: 返回错误结果 {}'.format(requrl, respjson['errmsg']))
-                    return False
-                crawl_room_results = respjson['data']['items']
-                for room_json in crawl_room_results:
-                    room = channel.rooms.filter_by(officeid=room_json['id']).one_or_none()
-                    if not room:
-                        room = LiveTVRoom(officeid=room_json['id'])
-                        current_app.logger.info('新增房间 {}:{}'.format(room_json['id'], room_json['name']))
-                    else:
-                        current_app.logger.info('更新房间 {}:{}'.format(room_json['id'], room_json['name']))
-                    room.channel = channel
-                    room.name = room_json['name']
-                    room.url = '{}/{}'.format(channel.site.url, room_json['id'])
-                    room.boardcaster = room_json['userinfo']['nickName']
-                    room.popularity = room_json['person_num']
-                    room.last_active = True
-                    room.last_crawl_date = datetime.utcnow()
-                    room_data = LiveTVRoomData(room=room, popularity=room.popularity)
-                    db.session.add(room, room_data)
-                crawl_room_count += len(crawl_room_results)
-                if len(crawl_room_results) < crawl_pagenum:
-                    break
+        while True:
+            requrl = ROOM_LIST_API.format(channel.short_name, crawl_pageno, crawl_pagenum)
+            resp = requests.get(requrl, headers=panda_headers)
+            if resp.status_code != requests.codes.ok:
+                current_app.logger.error('调用接口{}失败: 状态{}'.format(requrl, resp.status_code))
+                return False
+            try:
+                respjson = resp.json()
+            except ValueError:
+                current_app.logger.error('调用接口{}失败: 内容解析json失败'.format(requrl))
+                return False
+            if respjson['errno'] != 0:
+                current_app.logger.error('调用接口{}失败: 返回错误结果 {}'.format(requrl, respjson['errmsg']))
+                return False
+            crawl_room_results = respjson['data']['items']
+            for room_json in crawl_room_results:
+                room = channel.rooms.filter_by(officeid=room_json['id']).one_or_none()
+                if not room:
+                    room = LiveTVRoom(officeid=room_json['id'])
+                    current_app.logger.info('新增房间 {}:{}'.format(room_json['id'], room_json['name']))
                 else:
-                    crawl_pageno += 1
-            channel.range = crawl_room_count - channel.roomcount
-            channel.roomcount = crawl_room_count
-            channel.last_crawl_date = datetime.utcnow()
-            channel_data = LiveTVChannelData(channel=channel, roomcount=channel.roomcount)
-            db.session.add(channel, channel_data)
-            db.session.commit()
-            return True
-        finally:
-            webdriver_client.close()
-            webdriver_client.quit()
+                    current_app.logger.info('更新房间 {}:{}'.format(room_json['id'], room_json['name']))
+                room.channel = channel
+                room.name = room_json['name']
+                room.url = '{}/{}'.format(channel.site.url, room_json['id'])
+                room.boardcaster = room_json['userinfo']['nickName']
+                room.popularity = room_json['person_num']
+                room.last_active = True
+                room.last_crawl_date = datetime.utcnow()
+                room_data = LiveTVRoomData(room=room, popularity=room.popularity)
+                db.session.add(room, room_data)
+            crawl_room_count += len(crawl_room_results)
+            if len(crawl_room_results) < crawl_pagenum:
+                break
+            else:
+                crawl_pageno += 1
+        channel.range = crawl_room_count - channel.roomcount
+        channel.roomcount = crawl_room_count
+        channel.last_crawl_date = datetime.utcnow()
+        channel_data = LiveTVChannelData(channel=channel, roomcount=channel.roomcount)
+        db.session.add(channel, channel_data)
+        db.session.commit()
+        return True
 
     def _single_room(self, room):
-        webdriver_client = get_webdriver_client()
+        room_requrl = ROOM_API.format(room.officeid)
+        room_resp = requests.get(room_requrl, headers=panda_headers)
+        if room_resp.status_code != requests.codes.ok:
+            current_app.logger.error('调用接口{}失败: 状态{}'.format(room_requrl, room_resp.status_code))
+            return False
         try:
-            room_requrl = ROOM_API.format(room.officeid)
-            try:
-                webdriver_client.get(room_requrl)
-                pre_element = webdriver_client.find_element_by_tag_name('pre')
-            except TimeoutException:
-                current_app.logger.error('调用房间接口{}失败: 内容获取失败'.format(room_requrl))
-                return False
-            try:
-                room_respjson = json.loads(pre_element.get_attribute('innerHTML'))
-            except ValueError:
-                current_app.logger.error('调用房间接口{}失败: 内容解析json失败'.format(room_requrl))
-                return False
-            if room_respjson['errno'] != 0:
-                current_app.logger.error('调用房间接口{}失败: 返回错误结果{}'.format(room_requrl, room_respjson['errmsg']))
-                return False
-            room_respjson = room_respjson['data']['info']
-            room.name = room_respjson['roominfo']['name']
-            room.boardcaster = room_respjson['hostinfo']['name']
-            room.popularity = room_respjson['roominfo']['person_num']
-            room.follower = room_respjson['roominfo']['fans']
-            room.reward = int(room_respjson['hostinfo']['bamboos'])
-            room.last_active = True
-            room.last_crawl_date = datetime.utcnow()
-            room_data = LiveTVRoomData(room=room, popularity=room.popularity,
-                                       follower=room.follower, reward=room.reward)
-            db.session.add(room, room_data)
-            db.session.commit()
-            return True
-        finally:
-            webdriver_client.close()
-            webdriver_client.quit()
+            room_respjson = room_resp.json()
+        except ValueError:
+            current_app.logger.error('调用房间接口{}失败: 内容解析json失败'.format(room_requrl))
+            return False
+        if room_respjson['errno'] != 0:
+            current_app.logger.error('调用房间接口{}失败: 返回错误结果{}'.format(room_requrl, room_respjson['errmsg']))
+            return False
+        room_respjson = room_respjson['data']['info']
+        room.name = room_respjson['roominfo']['name']
+        room.boardcaster = room_respjson['hostinfo']['name']
+        room.popularity = room_respjson['roominfo']['person_num']
+        room.follower = room_respjson['roominfo']['fans']
+        room.reward = int(room_respjson['hostinfo']['bamboos'])
+        room.last_active = True
+        room.last_crawl_date = datetime.utcnow()
+        room_data = LiveTVRoomData(room=room, popularity=room.popularity,
+                                   follower=room.follower, reward=room.reward)
+        db.session.add(room, room_data)
+        db.session.commit()
+        return True
