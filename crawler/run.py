4648ac959892d312b327f34c4e020ee80ed6828d
taogeT <zwtzjd@gmail.com>
2017-03-12 17:35:54 +0800

    crawler script add param log file.

--- crawler/run.py
+++ crawler/run.py
@@ -9,8 +9,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Running multiple spiders in the same process.')
     parser.add_argument('--db-uri', dest='db_uri', action='store', default='',
                         help='set sqlalchemy database uri.')
-    parser.add_argument('--user-agents', dest='user_agents', action='store', default='',
+    parser.add_argument('--user-agents', dest='user_agents', action='store', default=None,
                         help='set user agents file path.')
+    parser.add_argument('--log-file', dest='log_file', action='store', default=None,
+                        help='set log file path.')
     parser.add_argument('--crawler', dest='crawler', action='append', nargs='*',
                         help='run crawler name.')
     args = parser.parse_args()
@@ -18,6 +20,7 @@ if __name__ == '__main__':
     settings = get_project_settings()
     settings.set('SQLALCHEMY_DATABASE_URI', args.db_uri)
     settings.set('USER_AGENT_FILE', args.user_agents)
+    settings.set('LOG_FILE', args.log_file)
 
     process = CrawlerProcess(settings)
     if args.crawler:
