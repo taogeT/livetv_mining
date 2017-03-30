3d13e899294f0569de74a265c3b87f294f8d29ca
taogeT <zwtzjd@gmail.com>
2017-03-30 11:22:33 +0800

    fix bug about quanmin image url.

--- crawler/gather/spiders/quanmin.py
+++ crawler/gather/spiders/quanmin.py
@@ -10,15 +10,15 @@ class QuanminSpider(Spider):
     name = 'quanmin'
     allowed_domains = ['quanmin.tv']
     start_urls = [
-        'http://www.quanmin.tv/json/categories/list.json'
+        'https://www.quanmin.tv/json/categories/list.json'
     ]
     custom_settings = {
         'SITE': {
             'code': 'quanmin',
             'name': '全民',
             'description': '全民直播_做年轻人爱看的直播',
-            'url': 'http://www.quanmin.tv',
-            'image': 'http://static.quanmin.tv/static/v2/module/widget/header/img/logo_e00f8d6.svg',
+            'url': 'https://www.quanmin.tv',
+            'image': 'https://static.quanmin.tv/static/v2/module/widget/header/img/logo_e00f8d6.svg',
             'show_seq': 7
         }
     }
@@ -33,7 +33,7 @@ class QuanminSpider(Spider):
                 'image': cjson['image'],
                 'url': response.urljoin('/game/{}'.format(cjson['slug'])),
             })
-            url = 'http://www.quanmin.tv/json/categories/{}/list{{}}.json'.format(cjson['slug'])
+            url = 'https://www.quanmin.tv/json/categories/{}/list{{}}.json'.format(cjson['slug'])
             room_query_list.append({'url': url, 'page': 0, 'channel': cjson['slug']})
         for room_query in room_query_list:
             yield Request(room_query['url'].format(''), callback=self.parse_room_list, meta=room_query)
@@ -43,10 +43,13 @@ class QuanminSpider(Spider):
             room_list = json.loads(response.text)['data']
             if isinstance(room_list, list):
                 for rjson in room_list:
+                    image_url = rjson['thumb'][:rjson['thumb'].find('?')]
+                    if image_url.startswith('http://'):
+                        image_url = image_url.replace('http://', 'https://')
                     yield RoomItem({
                         'office_id': rjson['id'],
                         'name': rjson['title'],
-                        'image': rjson['thumb'],
+                        'image': image_url,
                         'url': response.urljoin('/'+rjson['uid']),
                         'online': rjson['view'],
                         'host': rjson['nick'],
