d8255872c3cc0e979fdd1cf53086aac5cd6d2dea
zhengwentao <zwtzjd@gmail.com>
2017-04-06 03:04:52 -0500

    Update Intro.vue

--- frontend/src/components/Intro.vue
+++ frontend/src/components/Intro.vue
@@ -6,7 +6,7 @@
            站点按照<a href="http://www.alexa.com/" target="_blank">Alexa</a>统计流量排序。
         </p>
         <p>作者<a href="https://blog.zhengwentao.com/" target="_blank">taogeT</a>独立开发完成，
-           源代码受<a rel="license" href="https://github.com/taogeT/display_shelf/blob/master/LICENSE" target="_blank">GPL</a>开源协议保护，
+           源代码受<a rel="license" href="https://github.com/taogeT/display_shelf/blob/master/LICENSE" target="_blank">Apache-2.0</a>协议保护，
            地址 <a href="https://github.com/taogeT" target="_blank">GitHub 仓库</a>。
         </p>
       </div>
