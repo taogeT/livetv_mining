472b55335f791ec2f23466d9a818fb31499d93bf
taogeT <zwtzjd@gmail.com>
2017-05-03 23:45:00 +0800

    add quanmin total crawler.

---
+++ crawler/gather/daily_spiders/quanmin.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime, timedelta
+
+from ..models import LiveTVSite, LiveTVRoom, LiveTVRoomPresent, DAILY_DATE_FORMAT
+from ..items import DailyItem
+
+import numpy
+
+
+class DailySpider(Spider):
+    name = 'daily'
+    custom_settings = {}
+
+    def start_requests(self):
+        summary_utc = datetime.utcnow() - timedelta(days=1)
+        db_engine = create_engine(self.settings.get('SQLALCHEMY_DATABASE_URI'))
+        db_session = sessionmaker(bind=db_engine)()
+        db_query = db_session.query(LiveTVSite.id.label('site_id'), LiveTVRoom.id.label('room_id'),
+                                    LiveTVRoom.url.label('room_url'),
+                                    LiveTVRoomPresent.crawl_date_format.label('summary_date'),
+                                    func.array_agg(LiveTVRoomPresent.online).label('online_list'))\
+            .join(LiveTVSite, LiveTVRoom, LiveTVRoomPresent)\
+            .filter(LiveTVRoomPresent.crawl_date_format == summary_utc.strftime(DAILY_DATE_FORMAT))\
+            .group_by(LiveTVSite.id, LiveTVRoom.id, LiveTVRoom.url, LiveTVRoomPresent.crawl_date_format)
+        for group_row in db_query:
+            meta_info = {
+                'site_id': group_row.site_id,
+                'room_id': group_row.room_id,
+                'summary_date': group_row.summary_date,
+                'online': numpy.median(group_row.online_list)
+            }
+            room = self.session.query(LiveTVRoom).filter_by(id=meta_info['room_id']).one_or_none()
+            if room:
+                yield DailyItem(site_id=group_row.site_id, room_id=group_row.room_id,
+                                summary_date=group_row.summary_date, online=numpy.median(group_row.online_list),
+                                followers=room.followers, description=room.description, announcement=room.announcement,
+                                fallback=False)
+        db_session.close()
+
+    def parse(self, response):
+        raise NotImplementedError
---
+++ crawler/gather/once_spiders/quanmin.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider, Request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime, timedelta
+
+from ..models import LiveTVSite, LiveTVRoom
+from ..items import OnceItem
+
+import json
+
+
+class QuanminOnceSpider(Spider):
+    name = 'quanmin_once'
+    allowed_domains = ['quanmin.tv']
+
+    def start_requests(self):
+        db_engine = create_engine(self.settings.get('SQLALCHEMY_DATABASE_URI'))
+        db_session_maker = sessionmaker(bind=db_engine)
+        session = db_session_maker()
+        utc_date = datetime.utcnow()
+        base_query = session.query(LiveTVRoom).join(LiveTVSite).filter(LiveTVSite.code == 'panda')\
+                            .filter(LiveTVRoom.crawl_date <= utc_date)
+        for room in base_query.filter(LiveTVRoom.crawl_date > utc_date - timedelta(days=1)):
+            meta_info = {
+                'room_url': room.url,
+                'channel_name': room.channel.name,
+                'host': room.host,
+                'followers': room.followers,
+                'start_time': room.start_time,
+                'announcement': room.announcement
+            }
+            yield Request('http://www.quanmin.tv/json/rooms/{}/noinfo4.json'.format(room.office_id),
+                          callback=self.parse, meta=meta_info)
+        session.close()
+
+    def parse(self, response):
+        room_info = json.loads(response.text)
+        yield OnceItem({
+            'room_url': response.meta['room_url'],
+            'channel_name': response.meta['channel_name'],
+            'host': response.meta['host'],
+            'followers': response.meta['followers'],
+            'start_time': response.meta['start_time'],
+            'donate': str(room_info['weight']),
+            'description': room_info['intro'],
+            'announcement': response.meta['announcement'],
+        })
